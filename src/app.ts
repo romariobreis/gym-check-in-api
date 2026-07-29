94fb7ab0441bad2d31cc643f7813f7bb867c9a03
romario.reis <romario.breis@gmail.com>
2026-07-29 16:17:30 -0300

    feat: add refresh token

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,26 @@
 import fastify from "fastify";
+import fastifyJwt from "@fastify/jwt";
+import fastifyCookie from "@fastify/cookie";
 import { usersRoutes } from "./http/controllers/users/routes.js";
 import z, { ZodError } from "zod";
 import { env } from "./env/index.js";
-import fastifyJwt from "@fastify/jwt";
 import { gymsRoutes } from "./http/controllers/gyms/routes.js";
 import { checkInsRoutes } from "./http/controllers/check-ins/routes.js";
 
 export const app = fastify()
 
 app.register(fastifyJwt, {
-  secret: env.JWT_SECRET
+  secret: env.JWT_SECRET,
+  cookie: {
+    cookieName: 'refreshToken',
+    signed: false
+  },
+  sign: {
+    expiresIn: '10m'
+  }
 })
+
+app.register(fastifyCookie)
 
 app.register(usersRoutes)
 app.register(gymsRoutes)
